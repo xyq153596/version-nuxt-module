f57c763e93b48070eeb7fd315016e13f0773b65e
junjun <zhangjun0911@pinpianyi.com>
2019-06-03 14:34:48 +0800

    update

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const fse = require("fs-extra");
 const path = require("path");
 
 async function asyncModule() {
-  const { buildEnv } = this.options.global;
-  if (buildEnv === "dev") {
+  const { BUILD_ENV, IS_BUILD_DEV } = this.options.env;
+  if (IS_BUILD_DEV) {
     this.nuxt.hook("generate:done", async nuxt => {
       const nextVersion = nuxt.options.project.version.nextDev;
       const pkgPath = path.join(nuxt.options.rootDir, "package.json");
       const pkg = await fse.readJson(pkgPath);
-      pkg.project.version.dev = nextVersion;
+      pkg.project.version[BUILD_ENV] = nextVersion;
       await fse.outputFile(pkgPath, JSON.stringify(pkg, null, "\t"));
     });
   }
